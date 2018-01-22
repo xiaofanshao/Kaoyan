8157b80b354ace94af2ad50f41997e5578cfa8c3
pan <404457041@qq.com>
2018-01-22 16:14:14 +0800

    change encoding

---
+++ kaoyan/fushi/Ceui.java
@@ -0,0 +1,23 @@
+package kaoyan.fushi;
+
+public class Ceui {
+/**
+ * 
+ * @param args
+ */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=0;
+		int j=0;
+		for(;i<4;i++);
+		System.out.println(i);
+		
+		for(;i<4;);
+		System.out.println(i);
+		
+		while(++j<4);
+		System.out.println(j);
+		
+	}
+
+}
